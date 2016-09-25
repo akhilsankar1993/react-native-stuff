b3f64e4b66dde596e88ec370a3690a9da4ddbfd3
Akhil Sankar <akhil.sankar@gmail.com>
2016-09-25 17:14:33 -0600

    setup flexbox layout

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 #### Basic React Native Application Exercises (for iOS)
 
 - Exercise 1: Generating basic days of the week list (current day first)
-![Days of the Week](./days-of-the-week.png)
+![Days of the Week](./days_of_the_week.png)
 
 - Exercise 2: Stopwatch App
--- stopwatch/index.ios.js
+++ stopwatch/index.ios.js
@@ -1,14 +1,76 @@
 import React, { Component } from 'react'
-import { View, Text, AppRegistry } from 'react-native'
+import { View, Text, AppRegistry, StyleSheet } from 'react-native'
 
 class StopWatch extends Component {
   render() {
     return (
-      <Text>
-        This works!
-      </Text>
+      <View style={styles.container}>
+
+        <View style={[styles.header, this.border('yellow')]}>
+          <View style={this.border('red')}>
+            <Text>
+              00:00:00
+            </Text>
+          </View>
+          <View style={this.border('green')}>
+            { this.startStopButton() }
+            { this.lapButton() }
+          </View>
+        </View>
+
+        <View style={[styles.footer, this.border('blue')]}>
+          <Text>
+            I am a list of laps
+          </Text>
+        </View>
+
+      </View>
     )
   }
+
+  startStopButton() {
+    return(
+      <View>
+        <Text>
+          Start
+        </Text>
+      </View>
+    )
+  }
+
+  lapButton() {
+    return(
+      <View>
+        <Text>
+          Lap
+        </Text>
+      </View>
+    )
+  }
+
+  border(color) {
+    return {
+      borderColor: color,
+      borderWidth: 4
+    }
+  }
+
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'stretch'
+  },
+
+  header: { //yellow
+    flex: 1
+  },
+
+  footer: { //blue
+    flex: 1
+  }
+})
+
 
 AppRegistry.registerComponent('stopwatch', () => StopWatch)
